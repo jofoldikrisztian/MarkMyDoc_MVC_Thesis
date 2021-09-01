842048a7b437ef010c237f0c2c56e0a4df6cd67e
jofoldikrisztian <krisztianjofoldi@gmail.com>
2021-09-01 11:44:24 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MarkMyDoctor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
